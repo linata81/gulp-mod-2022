06e96161151d9c007dfb8cc513ed51870548d754
Natalia <linata81@gmail.com>
2022-06-19 16:53:28 +0300

    added Accordeon

--- component/Accordeon/Accordeon.js
+++ component/Accordeon/Accordeon.js
@@ -1,25 +1,3 @@
-import * as flsFunctions from "./modules/functions.js";
-flsFunctions.isWebp();
-
-//hamburger menu
-(() => {
-  const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".menu");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
-
-document.querySelectorAll(".menu__link").forEach(n => n.addEventListener("click", (e) => {
-  const isDropdownButton = e.target.matches("[data-dropdown-button]");
-  if(!isDropdownButton) {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");  
-  }
-}))
-})();
-
 //Accordeon
 (() => {
   document.querySelectorAll('.accordeon__header').forEach(el => {
