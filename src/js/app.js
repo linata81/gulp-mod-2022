774348cdd2949c33ca78218fa762ec7c27b3abfd
Natalia <linata81@gmail.com>
2022-06-25 09:51:50 +0300

    fixed hamburger-menu

--- src/js/app.js
+++ src/js/app.js
@@ -9,13 +9,19 @@ const navMenu = document.querySelector(".menu");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  if(document.body.style.overflow ==="hidden") {
+    document.body.style.overflow ="visible"
+  }else {
+    document.body.style.overflow ="hidden"
+  }
 });
 
 document.querySelectorAll(".menu__link").forEach(n => n.addEventListener("click", (e) => {
   const isDropdownButton = e.target.matches("[data-dropdown-button]");
   if(!isDropdownButton) {
     hamburger.classList.remove("active");
-    navMenu.classList.remove("active");  
+    navMenu.classList.remove("active"); 
+    document.body.style.overflow="visible" 
   }
 }))
 })();
