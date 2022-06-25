9284c8e3d6c5a93fc02dc2ab9a962c32993c297a
Natalia <linata81@gmail.com>
2022-06-25 21:24:09 +0300

    fixed navbar

--- src/js/app.js
+++ src/js/app.js
@@ -4,26 +4,46 @@ flsFunctions.isWebp();
 //hamburger menu
 (() => {
   const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".menu");
+  const navMenu = document.querySelector(".menu");
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-  if(document.body.style.overflow ==="hidden") {
-    document.body.style.overflow ="visible"
-  }else {
-    document.body.style.overflow ="hidden"
-  }
-});
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    document.body.classList.toggle('locked');
+  });
 
-document.querySelectorAll(".menu__link").forEach(n => n.addEventListener("click", (e) => {
-  const isDropdownButton = e.target.matches("[data-dropdown-button]");
-  if(!isDropdownButton) {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active"); 
-    document.body.style.overflow="visible" 
+  document.querySelectorAll(".menu__link").forEach(n => n.addEventListener("click", (e) => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]");
+    if(!isDropdownButton) {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active"); 
+      document.body.classList.remove("locked");
+    }
+  }))
+  
+//если телефон повернут вертикально и экран расширится, чтобы сбросить все блокировки
+  window.addEventListener('resize', () => {
+    if(window.innerWidth > 768) {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active"); 
+      document.body.classList.remove("locked");
+    }
+  })
+  
+//если нужно зафиксировать меню при скроле
+  function fixedNav() {
+    const nav = document.querySelector(".navbar");
+    
+    //указать в пикселях, сколько проскролить чтобы меню стало фиксированным
+    const breakpoint = 1;
+    if(window.scrollY >= breakpoint) {
+      nav.classList.add("fixed-nav")
+    } else {
+      nav.classList.remove("fixed-nav")
+    }
   }
-}))
+  
+  window.addEventListener('scroll', fixedNav)
 })();
 
 
